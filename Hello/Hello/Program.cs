71276b41abe9a4625f7f849dc6124676eacfcb4d
lee jaebyeok <dlwoqur159@naver.com>
2021-07-25 20:53:29 +0900

    first push

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace Hello
+{
+    class Program
+    {
+        //프로그램이 시작되는 곳
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Hello World!");
+                return;
+            }
+
+            WriteLine("Hello, {0!", args[0]);
+        }
+    }
+}
+ 
